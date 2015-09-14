0c3a8a005ad177dfb15ec3c593912eaace198914
Jennifer Miller <jennifer.a.miller2@gmail.com>
2015-09-13 23:39:15 -0400

    Move logic for updating chains into CompletedDay model method

--- app/controllers/completed_days_controller.rb
+++ app/controllers/completed_days_controller.rb
@@ -14,21 +14,7 @@ class CompletedDaysController < ApplicationController
     if @completed_day.save
       redirect_to action: "index"
       
-      completed_days = CompletedDay.all.sort_by(&:date)
-
-      chains = completed_days.slice_before { |completed_day|
-        index = completed_days.index(completed_day)
-        completed_days[index-1].date != completed_day.date-1.day
-      }
-
-      Chain.destroy_all
-      @chains = chains.map { |chain| 
-        Chain.create(:start_date => chain[0].date, :end_date => chain[-1].date, :current => FALSE)
-      }
-      # TODO:
-      # assign current chain
-      # apply this method on deleting a completed day as well
-      # try sidekiq for async
+      CompletedDay.update_chains
     else
       render 'new'
     end
--- app/models/completed_day.rb
+++ app/models/completed_day.rb
@@ -1,3 +1,21 @@
 class CompletedDay < ActiveRecord::Base
   belongs_to :chain
+
+  def self.update_chains
+    completed_days = CompletedDay.all.sort_by(&:date)
+
+        chains = completed_days.slice_before { |completed_day|
+          index = completed_days.index(completed_day)
+          completed_days[index-1].date != completed_day.date-1.day
+        }
+
+        Chain.destroy_all
+        @chains = chains.map { |chain| 
+          Chain.create(:start_date => chain[0].date, :end_date => chain[-1].date, :current => FALSE)
+        }
+        # TODO:
+        # assign current chain
+        # apply this method on deleting a completed day as well
+        # try sidekiq for async 
+  end
 end
