35737e7943b8687576eb3c125267aff51efddda8
Jennifer Miller <jennifer.a.miller2@gmail.com>
2015-09-15 13:58:20 -0400

    Add statement of previous longest chain to "your current chain" view.

--- app/controllers/chains_controller.rb
+++ app/controllers/chains_controller.rb
@@ -1,5 +1,8 @@
 class ChainsController < ApplicationController
   def index
     @chains = Chain.all
+
+    previous_longest_chain = Chain.where(current: FALSE).max_by{ |chain| chain.chain_length } 
+    @previous_longest_chain_length = previous_longest_chain.chain_length
   end
 end
--- app/models/chain.rb
+++ app/models/chain.rb
@@ -1,3 +1,8 @@
 class Chain < ActiveRecord::Base
   has_many :completed_days
+
+  def chain_length
+    (self[:end_date] - self[:start_date]).to_i + 1
+  end
+
 end
