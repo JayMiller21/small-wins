d118734d0d7a19adc8734f7c19f213e36e6a8b7d
Jennifer Miller <jennifer.a.miller2@gmail.com>
2015-09-15 14:09:40 -0400

    Add current chain dates to "your current chain" view

--- app/controllers/chains_controller.rb
+++ app/controllers/chains_controller.rb
@@ -4,5 +4,8 @@ class ChainsController < ApplicationController
 
     previous_longest_chain = Chain.where(current: FALSE).max_by{ |chain| chain.chain_length } 
     @previous_longest_chain_length = previous_longest_chain.chain_length
+
+    @current_chain = Chain.where(current: TRUE)[0]
+    @current_chain_dates = (@current_chain.start_date..@current_chain.end_date).map{ |date| date.strftime("%b %d") }
   end
 end
