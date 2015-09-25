7486d416ec03bcee202ae1dd7e2327b719a8ed79
Jennifer Miller <jennifer.a.miller2@gmail.com>
2015-09-25 14:41:07 -0400

    Update timelines to area charts with full month shown on axis - only test data currently implemented

--- app/assets/javascripts/habits.js
+++ app/assets/javascripts/habits.js
@@ -5,21 +5,36 @@
 var ready;
 ready = function() {
 
-  google.load('visualization', '1.0', { packages:["timeline"] });
+  // google.load('visualization', '1.0', { packages:["timeline"] });
   google.setOnLoadCallback(habits.forEach(drawChart));
 
   function drawChart(habit,index,array) {
     var container = document.getElementById(habit[0] + '-timeline');
-    var chart = new google.visualization.Timeline(container);
-    var dataTable = new google.visualization.DataTable();
+    var chart = new google.visualization.AreaChart(container);
+    // var dataTable = new google.visualization.DataTable();
+    var d = new Date();
 
-    dataTable.addColumn({ type: 'string', id: 'President' });
-    dataTable.addColumn({ type: 'date', id: 'Start' });
-    dataTable.addColumn({ type: 'date', id: 'End' });
-    dataTable.addRows(
-      [habit]);
+    var data = google.visualization.arrayToDataTable([
+          ['Date', 'HabCompleted'],
+          [new Date(), 1],
+          [new Date(2015,8,27), 1]
+        ]);
 
-    chart.draw(dataTable);
+    // dataTable.addColumn({ type: 'string', id: 'President' });
+    // dataTable.addColumn({ type: 'date', id: 'Start' });
+    // dataTable.addColumn({ type: 'date', id: 'End' });
+    // dataTable.addRows(
+    //   [habit]);
+
+    chart.draw(data,{
+      hAxis: { 
+        viewWindowMode:'explicit',
+        viewWindow:{
+          max: new Date(d.getFullYear(),d.getMonth()+1,0),
+          min: new Date(d.getFullYear(),d.getMonth(),1)
+        }
+      }
+    });
   }
 
 };
